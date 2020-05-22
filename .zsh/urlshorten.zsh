36a5e00274faba7fc87a4ea44b74d80927fb9390
araj <ja.fileonly@gmail.com>
2020-05-23 00:40:34 +0900

    Delete urlshorten.zsh

--- .zsh/urlshorten.zsh
+++
@@ -1,4 +0,0 @@
-urls() {
-  curl -sS "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyB7eVjm0ulrcNMq0-ySu_sQ4Cdyl1YMJ-k" -X POST -H "Content-Type: application/json" -d "{\"longDynamicLink\": \"https://jahost.page.link/?link=$1\"}" | jq -r '.shortLink'
-}
-
